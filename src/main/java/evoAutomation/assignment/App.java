f143720203397115583da419ab2eb3a5b599d93d
prakrick <prakrick@gmail.com>
2021-05-22 14:53:23 +0530

    create new maven project repo

---
+++ src/main/java/evoAutomation/assignment/App.java
@@ -0,0 +1,13 @@
+package evoAutomation.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
